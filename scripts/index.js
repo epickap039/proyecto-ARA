2f2d2986612de20b14c0c78a45f5fa14cc087205
epickap039 <alejandrodjghz@gmail.com>
2023-02-15 17:42:23 -0600

    Boton de eliminar producto con funcionalidad correcta

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,7 @@ const contadorcarrito = document.createElement("p");
 
 
 
+
 //---------Botones del Menu Hamburguesa------------------------------------
 const nav = document.getElementById("nav");
 const abrir = document.getElementById("abrir");
@@ -36,81 +37,21 @@ const agregarLocalstorage = (clave, valor) => {
     localStorage.setItem(clave, JSON.stringify(valor));
 }
 
-// const eliminarLocalStorage = (clave, valor) => {
-//     localStorage.removeItem(clave, JSON.stringify(valor));
-// }
-
-// const eliminartodoLocalStorage = localStorage.clear();
-
-// function Cantidad() {
-//     itemLocalStorage = JSON.parse(localStorage.getItem('carrito'));
-//     console.log("este viene de el local  " + itemLocalStorage);
-// }
-
-
-const mostrarCarrito2 = () => {
-    itemLocalStorage = JSON.parse(localStorage.getItem('carrito'));
-    // console.log("este viene de el local  " + itemLocalStorage);
-    contenedorCarritoCA.innerHTML = "";
-    itemLocalStorage.forEach(servicio => {
-        const div = document.createElement("div");
-        div.classList.add("itemcarrito");
-        div.innerHTML +=
-            `
-            <img class="imagenbote" src="${servicio.img}" alt="imagen de ${servicio.nombre}">
-            <p>${servicio.nombre}</p>
-            <p>$ ${servicio.precio} MXN</p>
-            <p>Cantidad</p>
-            <button class="eliminarcarrito" id="eliminardelcarrito${servicio.id}">
-            <img class="iconbote" src="imagenes/bote.png" alt="imagen de ${servicio.nombre}">
-            </button>
-            `
-        contenedorCarritoCA.appendChild(div);
-        // const botonBorrarCarrito = document.getElementById(`eliminardelcarrito${servicio.id}`)
-        // botonBorrarCarrito.addEventListener("click", () => {
-        //     eliminarItemdeCarrito(servicio.id, carritoCompras);
-        //     //estos no deberian estar aqui pero mientras veo como eliminar un item por separado necesito ver si funcionan
-        //     AgregarContador();
-        //     mostrarCarrito2();
-        // });
-    });
-}
-
-
-
-
 //---------------------Productos------------------------------------------------
 const seviciosDesarrollador = [
-    { id: 1, img: "../imagenes/brazo robot.jfif", nombre: "Brazo robot", precio: 20000, descripcion: "Brazo robot con 4 grados de libertad listo para uso personal/industrial" },
-    { id: 2, img: "../imagenes/leds.jpg", nombre: "Leds Personalizados", precio: 500, descripcion: "Proyectos complejos o para uso personal autonomo" },
-    { id: 3, img: "../imagenes/audifonos.jpg", nombre: "Audifonos ", precio: 2500, descripcion: "Audifonos recomendados por nuestros patrocinadores" },
-    { id: 4, img: "../imagenes/im5.png", nombre: "Drones", precio: 6500, descripcion: "Drones con distintas caracteristicas segun sus necesidades" },
-    { id: 5, img: "../imagenes/teclado.jpg", nombre: "Teclados ", precio: 1500, descripcion: "Teclados recomendados por nuestros patrocinadores" },
-    { id: 6, img: "../imagenes/placa.jpg", nombre: "PLaca PCB ", precio: 500, descripcion: "Placa personalizada segun requiera, el costo aumenta segun los componentes" },
-    { id: 7, img: "../imagenes/carro.jpg", nombre: "Vehiculos Personalizados ", precio: 3500, descripcion: "El vehiculo que siepre deseo pero en miniatura o no? " },
+    { id: 1, img: "../imagenes/brazo robot.jfif", nombre: "Brazo robot", precio: 20000, descripcion: "Brazo robot con 4 grados de libertad listo para uso personal/industrial", cantidad: 1 },
+    { id: 2, img: "../imagenes/leds.jpg", nombre: "Leds Personalizados", precio: 500, descripcion: "Proyectos complejos o para uso personal autonomo", cantidad: 1},
+    { id: 3, img: "../imagenes/audifonos.jpg", nombre: "Audifonos ", precio: 2500, descripcion: "Audifonos recomendados por nuestros patrocinadores", cantidad: 1 },
+    { id: 4, img: "../imagenes/im5.png", nombre: "Drones", precio: 6500, descripcion: "Drones con distintas caracteristicas segun sus necesidades", cantidad: 1 },
+    { id: 5, img: "../imagenes/teclado.jpg", nombre: "Teclados ", precio: 1500, descripcion: "Teclados recomendados por nuestros patrocinadores", cantidad: 1 },
+    { id: 6, img: "../imagenes/placa.jpg", nombre: "PLaca PCB ", precio: 500, descripcion: "Placa personalizada segun requiera, el costo aumenta segun los componentes", cantidad: 1 },
+    { id: 7, img: "../imagenes/carro.jpg", nombre: "Vehiculos Personalizados ", precio: 3500, descripcion: "El vehiculo que siepre deseo pero en miniatura o no? ", cantidad: 1 },
 
 ];
 
 //----------------------------------------------------------------------------------------------------
-const carritoCompras = [];
-
-
-const agregarAlcarrito = (servicioSeleccionado, carrito) => {
-    const servicioElegido = seviciosDesarrollador.find(item => item.id === servicioSeleccionado)
-    carrito.push(servicioElegido);
-    console.log("se agrego un producto al carrito", carritoCompras);
-    agregarLocalstorage("carrito", carritoCompras);
-    showAlert();
-}
-
-
-
-// const eliminarItemdeCarrito = (servicioSeleccionado, carrito) => {
-//     const servicioElegidoEliminar = itemLocalStorage.find(item => item.id === servicioSeleccionado)
-//     carrito.push(servicioElegidoEliminar);
-//     eliminarLocalStorage("carrito", carritoCompras);
-// }
-
+let carritoCompras = [];
+let mostrarEliminar = [];
 
 //Contador de Items en el carrito
 const AgregarContador = () => {
@@ -121,28 +62,31 @@ const AgregarContador = () => {
     }
 }
 
-//Mostrar en OFFcanvas=====================================================
+// Mostrar en OFFcanvas=====================================================
 
-// const mostrarCarrito = () => {
-//     contenedorCarritoCA.innerHTML = "";
-//     carritoCompras.forEach(servicio => {
-//         const div = document.createElement("div");
-//         div.classList.add("itemcarrito");
-//         div.innerHTML +=
-//             `
-//             <img class="imagenbote" src="${servicio.img}" alt="imagen de ${servicio.nombre}">
-//             <p>${servicio.nombre}</p>
-//             <p>$ ${servicio.precio} MXN</p>
-//             <p>Cantidad</p>
-//             <button class="eliminarcarrito" id="eliminardelcarrito${servicio.id}">
-//             <img class="iconbote" src="imagenes/bote.png" alt="imagen de ${servicio.nombre}">
-//             </button>
-//             `
-//         contenedorCarritoCA.appendChild(div);
-//     })
-// }
-
-
+const mostrarCarrito = () => {
+    contenedorCarritoCA.innerHTML = "";
+    carritoCompras.forEach(servicio => {
+        const div = document.createElement("div");
+        div.classList.add("itemcarrito");
+        div.innerHTML =
+            `
+        <img class="imagenbote" src="${servicio.img}" alt="imagen de ${servicio.nombre}">
+        <p>${servicio.nombre}</p>
+        <p>$ ${servicio.precio} MXN</p>
+        <p>Cantidad</p>
+        `
+        contenedorCarritoCA.appendChild(div);
+        let eliminar = document.createElement("div");
+        eliminar.innerHTML = `<img class="iconbote" src="imagenes/bote.png" alt="imagen de ${servicio.nombre}">`
+        eliminar.className = `eliminarcarrito${servicio.id}`;
+        div.append(eliminar);
+        eliminar.addEventListener("click", () =>{
+            eliminarProducto(servicio.id, carritoCompras);
+            // console.log("producto para eliminar"+ JSON.stringify( carritoCompras));
+        });
+    })
+}
 
 //agregar al carrito en index
 
@@ -162,10 +106,27 @@ seviciosDesarrollador.forEach(servicio => {
     const botonAgregarCarrito = document.getElementById(`producto${servicio.id}`)
     botonAgregarCarrito.addEventListener("click", () => {
         agregarAlcarrito(servicio.id, carritoCompras);
+        console.log("este es el de agregar al carrito " + servicio.id)
         AgregarContador();
-        mostrarCarrito2();
-        // Cantidad();
+        mostrarCarrito();
+
     })
 });
 
+const agregarAlcarrito = (servicioSeleccionado, carrito) => {
+    const servicioElegido = seviciosDesarrollador.find(item => item.id === servicioSeleccionado)
+    carrito.push(servicioElegido);
+    showAlert();
+}
+
+const eliminarProducto = (servicioSeleccionado, carrito) => {
+    const BuscarID = carritoCompras.find((elmeento) => elmeento.id === servicioSeleccionado);
+    carritoCompras = carritoCompras.filter((carritoID) => {
+        return carritoID !== BuscarID;
+    })
+    carrito.push(carritoCompras);
+    mostrarCarrito();
+}
+
+//=====================================================================================================================================
 
